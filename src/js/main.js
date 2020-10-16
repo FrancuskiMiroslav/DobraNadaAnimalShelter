68f004972e536fb571a6463fff5f860ff776f642
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-10-16 23:02:48 +0200

    first commit, done header

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const navContainer = document.getElementById('nav-container');
+	const menuBtn = document.getElementById('menu-btn');
+	const navList = document.getElementById('nav-list');
+
+	window.addEventListener('scroll', (e) => {
+		if (document.documentElement.scrollTop > 0) {
+			navContainer.classList.add('sticky');
+		} else {
+			navContainer.classList.remove('sticky');
+		}
+	});
+
+	menuBtn.addEventListener('click', (e) => {
+		menuBtn.classList.toggle('clicked');
+		navContainer.classList.toggle('overlay');
+		navList.classList.toggle('show');
+	});
+});
