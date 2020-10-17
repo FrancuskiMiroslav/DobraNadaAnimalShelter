26ae6cec8259cd4c58888d45fcf745db0770ce1c
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-10-17 18:52:36 +0200

    addded scroll to top btn

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
 		preload.classList.add('preload-finished');
 	});
 
+	const btnScrollToTop = document.getElementById('btnScrollToTop');
+
+	btnScrollToTop.addEventListener('click', (e) => {
+		window.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: 'smooth',
+		});
+	});
+
 	const navContainer = document.getElementById('nav-container');
 	const menuBtn = document.getElementById('menu-btn');
 	const navList = document.getElementById('nav-list');
@@ -12,8 +22,10 @@ document.addEventListener('DOMContentLoaded', function () {
 	window.addEventListener('scroll', (e) => {
 		if (document.documentElement.scrollTop > 0) {
 			navContainer.classList.add('sticky');
+			btnScrollToTop.style.opacity = 1;
 		} else {
 			navContainer.classList.remove('sticky');
+			btnScrollToTop.style.opacity = 0;
 		}
 	});
 
@@ -22,4 +34,99 @@ document.addEventListener('DOMContentLoaded', function () {
 		navContainer.classList.toggle('overlay');
 		navList.classList.toggle('show');
 	});
+
+	// carousel
+	const carousel = document.getElementById('carousel');
+	const prevBtn = document.getElementById('prev');
+	const nextBtn = document.getElementById('next');
+	const carouselUrl = './js/carousel.json';
+
+	let firstSlide;
+	let lastSlide;
+
+	function getJsonData() {
+		return new Promise((resolve, reject) => {
+			fetch(carouselUrl)
+				.then((resp) => resp.json())
+				.then((data) => resolve(data))
+				.catch((err) => reject(err));
+		});
+	}
+
+	async function displayCarousel() {
+		const dataList = await getJsonData();
+		const slidesArray = dataList.slides;
+
+		slidesArray.forEach((slide, slideNumber) => {
+			newSlide = document.createElement('div');
+			newSlide.classList.add('carousel__slide');
+
+			newSlide.innerHTML = `
+						<div class="carousel__image-container">
+							<img src="${slide.image}" alt="slide-img" />
+						</div>
+						<div class="carousel__content">
+							<h1 class="carousel__title">${slide.title}</h1>
+							<p class="carousel__desc">${slide.content}</p>
+							<button class="header__nav-donate-btn button">
+								Dugme za nešto
+							</button>
+						</div>
+			`;
+
+			carousel.appendChild(newSlide);
+
+			if (slideNumber === 0) {
+				firstSlide = newSlide;
+				newSlide.classList.add('active');
+			} else if (slideNumber + 1 === dataList.slides.length) {
+				lastSlide = newSlide;
+			}
+		});
+
+		nextSlide();
+		previousSlide();
+	}
+
+	function nextSlide() {
+		const nextBtn = document.getElementById('next');
+
+		nextBtn.addEventListener('click', (e) => {
+			const activeSlide = document.querySelector('.carousel__slide.active');
+
+			let nextSibling = activeSlide.nextElementSibling;
+
+			if (nextSibling == null) {
+				nextSibling = activeSlide;
+			}
+
+			if (nextSibling.classList.contains('carousel__slide')) {
+				activeSlide.classList.remove('active');
+				nextSibling.classList.add('active');
+			}
+		});
+	}
+
+	function previousSlide() {
+		const prevBtn = document.getElementById('prev');
+
+		prevBtn.addEventListener('click', (e) => {
+			const activeSlide = document.querySelector('.carousel__slide.active');
+
+			let nextSibling = activeSlide.previousElementSibling;
+
+			if (nextSibling == null) {
+				nextSibling = activeSlide;
+			}
+
+			if (nextSibling.classList.contains('carousel__slide')) {
+				activeSlide.classList.remove('active');
+				nextSibling.classList.add('active');
+			}
+		});
+	}
+
+	(function () {
+		displayCarousel();
+	})();
 });
