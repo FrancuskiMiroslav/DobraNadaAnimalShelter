66d67e11c4969905b997b75911b1dabfbcb1e410
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-10-26 21:19:57 +0100

    fixed responsive tabccordion and sticky nav positi

--- src/js/main.js
+++ src/js/main.js
@@ -135,58 +135,92 @@ document.addEventListener('DOMContentLoaded', function () {
 	// tabs
 	const tabListBtn = document.querySelectorAll('.tabccordion__list-btn');
 	const tabs = document.querySelectorAll('.tabccordion__container');
+	const tabccordionList = document.getElementById('tabccordion-list');
+	const tabsContent = document.querySelectorAll('.tabccordion__tab-inner');
 
-	tabListBtn.forEach((tab) => {
-		tab.addEventListener('click', (e) => {
-			const tabNav = tab.parentElement;
-			const tabsContainer = tabNav.parentElement.parentElement;
-			const tabNumber = tab.dataset.forTab;
-			const tabToActivate = tabsContainer.querySelector(
-				`.tabccordion__tab[data-tab="${tabNumber}"]`
-			);
+	function activateTabs() {
+		tabListBtn.forEach((tab) => {
+			tab.addEventListener('click', (e) => {
+				const tabNav = tab.parentElement;
+				const tabsContainer = tabNav.parentElement.parentElement;
+				const tabNumber = tab.dataset.forTab;
+				const tabToActivate = tabsContainer.querySelector(
+					`.tabccordion__tab[data-tab="${tabNumber}"]`
+				);
 
-			tabNav.querySelectorAll('.tabccordion__list-btn').forEach((btn) => {
-				btn.classList.remove('current');
+				tabNav.querySelectorAll('.tabccordion__list-btn').forEach((btn) => {
+					btn.classList.remove('current');
+				});
+
+				tabsContainer.querySelectorAll('.tabccordion__tab').forEach((tab) => {
+					tab.classList.remove('current');
+				});
+
+				tab.classList.add('current');
+				tabToActivate.classList.add('current');
+
+				if (window.innerWidth >= 600) {
+					tabsContent.forEach((tabs) => {
+						tabs.style.maxHeight = tabs.scrollHeight + 'px';
+					});
+				}
 			});
+		});
 
-			tabsContainer.querySelectorAll('.tabccordion__tab').forEach((tab) => {
-				tab.classList.remove('current');
+		if (tabccordionList) {
+			tabs.forEach((tab) => {
+				tab.querySelector('.tabccordion__list .tabccordion__btn').click();
 			});
-
-			tab.classList.add('current');
-			tabToActivate.classList.add('current');
-		});
-	});
-
-	if (tabs) {
-		tabs.forEach((tab) => {
-			tab.querySelector('.tabccordion__list .tabccordion__btn').click();
-		});
+		}
 	}
 
 	///// accordion
-	const accordionBtn = document.querySelectorAll('.accordion__list-btn');
+	function activateAccordion() {
+		const accordionBtn = document.querySelectorAll('.accordion__list-btn');
 
-	accordionBtn.forEach((btn) => {
-		btn.addEventListener('click', () => {
-			// check for open accordion
-			const accordionBtnOpen = document.querySelector(
-				'.accordion__list-btn.open'
-			);
-			if (accordionBtnOpen && accordionBtnOpen !== btn) {
-				// if there is open then remove open class and set maxheight to 0
-				accordionBtnOpen.classList.toggle('open');
-				accordionBtnOpen.nextElementSibling.style.maxHeight = 0;
-			}
+		accordionBtn.forEach((btn) => {
+			btn.addEventListener('click', () => {
+				// check for open accordion
+				const accordionBtnOpen = document.querySelector(
+					'.accordion__list-btn.open'
+				);
 
-			// set open class to every clicked accordion btn
-			btn.classList.toggle('open');
-			const accordionBody = btn.nextElementSibling;
-			if (btn.classList.contains('open')) {
-				accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
-			} else {
-				accordionBody.style.maxHeight = 0;
-			}
+				if (accordionBtnOpen && accordionBtnOpen !== btn) {
+					// if there is open then remove open class and set maxheight to 0
+					accordionBtnOpen.classList.toggle('open');
+					accordionBtnOpen.nextElementSibling.style.maxHeight = 0;
+				}
+
+				// set open class to every clicked accordion btn
+				btn.classList.toggle('open');
+				const accordionBody = btn.nextElementSibling;
+
+				console.log(btn);
+
+				if (btn.classList.contains('open')) {
+					accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
+				} else {
+					accordionBody.style.maxHeight = 0;
+				}
+			});
 		});
+	}
+
+	if (window.innerWidth >= 600) {
+		activateTabs();
+	} else {
+		activateAccordion();
+	}
+
+	window.addEventListener('resize', (e) => {
+		if (window.innerWidth >= 600) {
+			activateTabs();
+			activateAccordion();
+		} else {
+			tabsContent.forEach((tabs) => {
+				tabs.style.maxHeight = 0;
+			});
+			activateAccordion();
+		}
 	});
 });
