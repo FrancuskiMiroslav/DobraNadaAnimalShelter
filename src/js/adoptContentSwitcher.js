bd5d46ea0cfb9e275d02858b784c8722db8368ad
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-11-01 17:25:02 +0100

    added responsive content switch adopt page

---
+++ src/js/adoptContentSwitcher.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+	history.replaceState(null, null, ' ');
+	const tabSwitchers = document.querySelectorAll('[data-switcher]');
+
+	tabSwitchers.forEach((tabSwitch) => {
+		const tabSwitcher = tabSwitch;
+		const pageId = tabSwitcher.dataset.tab;
+
+		tabSwitcher.addEventListener('click', (e) => {
+			document
+				.querySelector('.adopt__list-item.active')
+				.classList.remove('active');
+
+			tabSwitcher.parentElement.parentElement.classList.add('active');
+
+			switchPage(pageId);
+		});
+	});
+
+	function switchPage(pageId) {
+		const currentPage = document.querySelector('.page.active');
+		const currentPageHeader = document.querySelector('.page-header.active');
+
+		currentPage.classList.remove('active');
+		currentPageHeader.classList.remove('active');
+
+		const nextPage = document.querySelector(`.page[data-page="${pageId}"]`);
+		const nextPageHeader = document.querySelector(
+			`.page-header[data-header="${pageId}"]`
+		);
+		nextPage.classList.add('active');
+		nextPageHeader.classList.add('active');
+	}
+});
